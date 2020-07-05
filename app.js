ff7f32cd51bc0c85c34cfb38d81ee963a51871e8
wei <fhaowei@yahoo.com.tw>
2020-07-05 12:10:40 +0800

    feat: install multer and add image upload feature

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   next()
 })
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
 
 app.listen(port, () => {
   db.sequelize.sync()
