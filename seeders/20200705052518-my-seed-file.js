999ebe6ae66aca4612b491d338f71df56c7d25af
wei <fhaowei@yahoo.com.tw>
2020-07-08 00:09:11 +0800

    Add category seed date

--- seeders/20200705052518-my-seed-file.js
+++ seeders/20200705052518-my-seed-file.js
@@ -37,13 +37,26 @@ module.exports = {
           image: faker.image.imageUrl(),
           description: faker.lorem.text(),
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          CategoryId: Math.floor(Math.random() * 5) + 1
         })
       ), {});
+
+    await queryInterface.bulkInsert('Categories',
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+        .map((item, index) =>
+          ({
+            id: index + 1,
+            name: item,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+        ), {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {})
+    await queryInterface.bulkDelete('Users', null, {});
     await queryInterface.bulkDelete('Restaurants', null, {});
+    await queryInterface.bulkDelete('Categories', null, {});
   }
 };
