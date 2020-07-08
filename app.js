511613f9454757f61613029052bc1c7e505cbefd
wei <fhaowei@yahoo.com.tw>
2020-07-09 00:11:43 +0800

    feat: add handlebar helper ifCond

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main',
+  helpers: require('./config/handelbars-helpers')
+}))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
