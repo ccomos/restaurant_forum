eafdfd54f58850799686ec23e5cdb36cc6671917
wei <fhaowei@yahoo.com.tw>
2020-07-08 23:11:08 +0800

    feat: add categories selector on admin create and edit page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -19,7 +19,12 @@ const adminController = {
   },
 
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return res.render('admin/create', { categories: categories })
+    })
   },
 
   postRestaurant: (req, res) => {
@@ -68,9 +73,15 @@ const adminController = {
   },
 
   editRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
-      return res.render('admin/create', { restaurant: restaurant })
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return Restaurant.findByPk(req.params.id, { raw: true, nest: true }).then(restaurant => {
+        return res.render('admin/create', { restaurant: restaurant, categories: categories })
+      })
     })
+
   },
 
   putRestaurant: (req, res) => {
