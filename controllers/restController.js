2e657308608840f4ca02aa4d9b56e14f613f35b4
wei <fhaowei@yahoo.com.tw>
2020-07-11 13:55:51 +0800

    feat: add restaurants index page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,19 @@
+const db = require('../models')
+const Restaurant = db.Restaurants
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ include: Category }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.dataValues,// ...展開運算子 (spread operator)
+        description: r.dataValues.description.substring(0, 50),
+        categoryName: r.Category.name
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 
