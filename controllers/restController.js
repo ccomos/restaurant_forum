a2b1a66b8eb8487dd9c0809d499bfec00440328c
wei <fhaowei@yahoo.com.tw>
2020-07-11 14:39:15 +0800

    feat: add filter on category navbar

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,7 +4,13 @@ const Category = db.Category
 
 const restController = {
   getRestaurants: (req, res) => {
-    Restaurant.findAll({ include: Category }).then(restaurants => {
+    let whereQuery = {}
+    let categoryId = ''
+    if (req.query.categoryId) {
+      categoryId = Number(req.query.categoryId)
+      whereQuery['CategoryId'] = categoryId
+    }
+    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
       const data = restaurants.map(r => ({
         ...r.dataValues,// ...展開運算子 (spread operator)
         description: r.dataValues.description.substring(0, 50),
