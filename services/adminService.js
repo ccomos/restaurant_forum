3c88041de94b10c8d168c6dd0f4e635eb9b8de75
wei <fhaowei@yahoo.com.tw>
2020-07-17 00:25:17 +0800

    refactor adminController.getRestaurants

---
+++ services/adminService.js
@@ -0,0 +1,17 @@
+const db = require('../models')
+const Restaurant = db.Restaurants
+const Category = db.Category
+
+const adminService = {
+  getRestaurants: (req, res, callback) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    }).then(restaurants => {
+      callback({ restaurants: restaurants })
+    })
+  },
+}
+
+module.exports = adminService
